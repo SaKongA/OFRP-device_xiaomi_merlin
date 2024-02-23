0e2a5685ca6fcaa0095df673bd3cfa64522fd257
ZhcnPanda <2623493447@qq.com>
2024-02-23 16:04:11 +0800

    merlin: Add data decryption for android 12

--- device.mk
+++ device.mk
@@ -15,3 +15,17 @@ TW_INCLUDE_FASTBOOTD := true
 # API
 PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Additional target Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
